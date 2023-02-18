45f87593c11a926f29877fba03f867a45460e70d
Libasse Laye MBENGUE <94873781+liilaye@users.noreply.github.com>
2023-02-18 00:37:24 +0000

    login page done

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                "DOLIBARR",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.lightBlue,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Username",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Enter Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: () {}, child: const Text(""))
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  gradient: const LinearGradient(
+                      colors: [Colors.lightBlue, Colors.blueAccent]),
+                ),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "CONNECT",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Icon(
+                Icons.fingerprint,
+                size: 60,
+                color: Colors.lightBlue,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                height: 30,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "DIC1 2023",
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
